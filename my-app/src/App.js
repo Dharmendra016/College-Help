a1c0cdd989f878f862832b280fade3fa0583011d
Dharmendra Singh Chaudhary <103047711+dharay02@users.noreply.github.com>
2023-02-16 21:29:57 +0545

    second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+// let a , b ; 
+// a = parseInt(prompt("Eneter the value of a"));
+// b = parseInt(prompt("Eneter the value of a"));   // parseInt is used to convert string to numebr
+
+// function sum(a , b){
+//   let c = a + b;
+//   return c;
+// }
+
+
 function App() {
   return (
-    <div className="nav-bar">
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Sign up</li>
-          <li>Login</li>
-        </ul>
-      </nav>
+    <div className="container">
+      <div className="nav-bar">
+        <nav >
+          <ul className="nav-items">
+            <li> <a href="#">Home</a> </li>
+            <li> <a href="#">About</a> </li>
+            <li> <a href="#">Sign up</a></li>
+            <li> <a href="#">Login</a> </li>
+          </ul>
+        </nav>
+      </div>
     </div>
+
   );
 }
 
